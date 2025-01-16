66e772f40df0fb9cbbcd1c39f029ff8e7b6ad1fd
mxnan <128995981+mxnan@users.noreply.github.com>
2025-01-16 23:12:37 +0530

    setup vite

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="h-screen w-full flex items-center justify-center">
-      helll
-    </div>
-  );
-}
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Canvas from "./components/canvas/canvas"
+
+function App() {
+
+
+  return (
+    <>
+    <div className="relative h-screen w-full flex items-center justify-center">
+      <Canvas />
+    </div>
+    </>
+  )
+}
+
+export default App
